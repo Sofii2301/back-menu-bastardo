c566644530fe75ae19eee4a633549e89afbeac48
Sofii2301 <sofialvarezrodero97@gmail.com>
2026-07-16 04:24:06 -0300

    feat: configure Google Sheets API integration

---
+++ src/config/google.js
@@ -0,0 +1,18 @@
+import { google } from "googleapis";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const auth = new google.auth.GoogleAuth({
+    keyFile: "credentials.json",
+    scopes: [
+        "https://www.googleapis.com/auth/spreadsheets"
+    ]
+});
+
+export const sheets = google.sheets({
+    version: "v4",
+    auth
+});
+
+export const spreadsheetId = process.env.SPREADSHEET_ID;
