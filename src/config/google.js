a308fe352a2abb849f9d27674e174a3ddbc7fec5
Sofii2301 <sofialvarezrodero97@gmail.com>
2026-07-16 09:40:32 -0300

    refactor: replace GoogleAuth with JWT

--- src/config/google.js
+++ src/config/google.js
@@ -1,21 +1,12 @@
 import { google } from "googleapis";
 import dotenv from "dotenv";
-
 dotenv.config();
 
-const auth = new google.auth.GoogleAuth({
-    credentials: {
-        client_email: process.env.GOOGLE_CLIENT_EMAIL,
-        private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, "\n")
-    },
-    scopes: [
-        "https://www.googleapis.com/auth/spreadsheets"
-    ]
+const auth = new google.auth.JWT({
+    email: process.env.GOOGLE_CLIENT_EMAIL,
+    key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+    scopes: ["https://www.googleapis.com/auth/spreadsheets"]
 });
 
-export const sheets = google.sheets({
-    version: "v4",
-    auth
-});
-
+export const sheets = google.sheets({ version: "v4", auth });
 export const spreadsheetId = process.env.SPREADSHEET_ID;
